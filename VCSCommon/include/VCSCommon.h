f8a942a3371b1b3c79f623a915a2ee89e760f6bf
Ivaylo Iliev <iiliev@nemetschek.bg>
2022-03-18 11:43:06 +0200

    Add public header

--- VCSCommon/include/VCSCommon.h
+++ VCSCommon/include/VCSCommon.h
@@ -6,6 +6,3 @@ FOUNDATION_EXPORT double VCSCommonVersionNumber;
 FOUNDATION_EXPORT const unsigned char VCSCommonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VCSCommon/PublicHeader.h>
-
-#include <ifaddrs.h>
-#import <VCSCommon/VCSCommon-Swift.h>
