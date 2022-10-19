c6b0c6e30dc72d39a42a1af805cae89d37a27541
Ivaylo Iliev <iiliev@nemetschek.bg>
2022-10-19 16:05:39 +0300

    Update VCSError.swift

--- VCSCommon/Utils/VCSError.swift
+++ VCSCommon/Utils/VCSError.swift
@@ -1,8 +1,9 @@
 import Foundation
 
-public enum VCSError: Error {
+public enum VCSError: Error, Equatable {
     case IllegalArgumentException(String)
     case GenericException(String)
     case UserCancelled
     case CapturedRoomIsNil
+    case OperationNotExecuted
 }
