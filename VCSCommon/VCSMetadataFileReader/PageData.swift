b377b4513964ffc4d00cb8dcbc80c4c30b25dff5
bkalchv <bkaltchev98@gmail.com>
2023-01-11 15:32:58 +0200

    adds DEFAULT_API to PageData.swift

--- VCSCommon/VCSMetadataFileReader/PageData.swift
+++ VCSCommon/VCSMetadataFileReader/PageData.swift
@@ -3,6 +3,7 @@ import UIKit
 
 @objc public class PageData: NSObject {
     @objc public static let DEFAULT_DEC_DIM_PRECISION: Int = 2
+    @objc public static let DEFAULT_DPI: Int = 72
     @objc public var name: String = ""
     @objc public var dpi: Int = 0
     @objc public var style: Int = 0
