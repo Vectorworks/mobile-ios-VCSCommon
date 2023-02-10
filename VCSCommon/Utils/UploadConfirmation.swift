f717dec3dc88aef2a80889d14b9b3ed59b743a11
Ivaylo Iliev <iiliev@nemetschek.bg>
2023-02-10 16:01:13 +0200

    MOBILE-6948

--- VCSCommon/Utils/UploadConfirmation.swift
+++ VCSCommon/Utils/UploadConfirmation.swift
@@ -2,20 +2,20 @@ import SwiftUI
 
 public struct UploadConfirmation: ViewModifier {
     @Binding var isPresented: Bool
+    @Binding var isUFCPresented: Bool
     @Binding var uploadFolder: VCSFolderResponse
+    
     @State var title: String
     @State var message: String
     @State var changeButtonName: String
     @State var uploadButtonName: String
     @State var uploadButtonAction: (() -> Void)?
     
-    @State private var showFolderChooser = false
-    
     public func body(content: Content) -> some View {
         content
             .confirmationDialog(title, isPresented: $isPresented, actions: {
                 Button {
-                    showFolderChooser.toggle()
+                    isUFCPresented.toggle()
                 } label: {
                     Text(changeButtonName)
                 }
@@ -29,14 +29,14 @@ public struct UploadConfirmation: ViewModifier {
                     Text(message + "\n\(uploadFolder.displayedPrefix)")
                 }
             })
-            .fullScreenCover(isPresented: $showFolderChooser, content: {
-                FolderChooser(routeData: FCRouteData(folder: uploadFolder), folderResult: $uploadFolder, isPresented: $showFolderChooser, parentIsPresented: $isPresented)
+            .fullScreenCover(isPresented: $isUFCPresented, content: {
+                FolderChooser(routeData: FCRouteData(folder: uploadFolder), folderResult: $uploadFolder, isPresented: $isUFCPresented, parentIsPresented: $isPresented)
             })
     }
 }
 
 public extension View {
-    func uploadConfirmationDialog(isPresented: Binding<Bool>, uploadFolder: Binding<VCSFolderResponse>, title: String, message: String, changeButtonName: String, uploadButtonName: String, uploadButtonAction: (() -> Void)?) -> some View {
-        modifier(UploadConfirmation(isPresented: isPresented, uploadFolder: uploadFolder, title: title, message: message, changeButtonName: changeButtonName, uploadButtonName: uploadButtonName, uploadButtonAction: uploadButtonAction))
+    func uploadConfirmationDialog(isPresented: Binding<Bool>, isUFCPresented: Binding<Bool>, uploadFolder: Binding<VCSFolderResponse>, title: String, message: String, changeButtonName: String, uploadButtonName: String, uploadButtonAction: (() -> Void)?) -> some View {
+        modifier(UploadConfirmation(isPresented: isPresented, isUFCPresented: isUFCPresented, uploadFolder: uploadFolder, title: title, message: message, changeButtonName: changeButtonName, uploadButtonName: uploadButtonName, uploadButtonAction: uploadButtonAction))
     }
 }
