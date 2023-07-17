995e56a151b956bfabc5b02eee24db96c69d0c24
Ivaylo Iliev <iiliev@nemetschek.bg>
2023-07-17 14:47:31 +0300

    MOBILE-7617

--- VCSCommon/Utils/DeviceShareUtils.swift
+++ VCSCommon/Utils/DeviceShareUtils.swift
@@ -1,5 +1,5 @@
 import Foundation
-import UIKit
+import CocoaLumberjackSwift
 
 public class DeviceShareUtils {
     public static func deviceShare(shareFilesURLs: [URL], sourceView: UIView, presenter: UIViewController, completion: (() -> Void)? = nil) {
@@ -14,5 +14,41 @@ public class DeviceShareUtils {
             shareActivity.popoverPresentationController?.permittedArrowDirections = .any
         }
         presenter.present(shareActivity, animated: true, completion: completion)
+    }
+    
+    public static func copyURLsWithNames(cellDataHolders: [VCSCellDataHolder]) -> [URL] {
+        let tempShareFilesAndURLs:[(FileAsset, URL)] = cellDataHolders.compactMap {
+            guard let fileAsset = ($0.assetData as? FileAsset), let localPathString = fileAsset.localPathString else { return nil }
+            return (fileAsset, URL(fileURLWithPath: localPathString))
+        }
+        
+        let shareFilesURLs:[URL] = DeviceShareUtils.copyURLsWithNames(tempFileAssetsPair: tempShareFilesAndURLs)
+        return shareFilesURLs
+    }
+    
+    public static func copyURLsWithNames(fileAssets: [FileAsset]) -> [URL] {
+        let tempShareFilesAndURLs:[(FileAsset, URL)] = fileAssets.compactMap {
+            guard let localPathString = $0.localPathString else { return nil }
+            return ($0, URL(fileURLWithPath: localPathString))
+        }
+        
+        let shareFilesURLs:[URL] = DeviceShareUtils.copyURLsWithNames(tempFileAssetsPair: tempShareFilesAndURLs)
+        return shareFilesURLs
+    }
+    
+    private static func copyURLsWithNames(tempFileAssetsPair: [(FileAsset, URL)]) -> [URL] {
+        var shareFilesURLs:[URL] = []
+        try? FileManager.default.removeItem(at: FileManager.AppShareExtDirectory)
+        tempFileAssetsPair.forEach {
+            let shareFileURL = FileManager.AppShareExtDirectory.appendingPathComponent($0.0.name)
+            do {
+                try FileManager.default.copyItem(at: $0.1, to: shareFileURL)
+                shareFilesURLs.append(shareFileURL)
+            } catch {
+                DDLogError("Error while coping file: \(error)")
+            }
+        }
+        
+        return shareFilesURLs
     }
 }
