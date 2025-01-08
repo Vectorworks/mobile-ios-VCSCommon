5ef8a760b55d2eda8161d52d274ef75c6bf48940
Ivaylo Iliev <iiliev@nemetschek.bg>
2025-01-08 20:47:48 +0200

    Update OAuth2 Logic

--- VCSCommon/VCSNetwork/AuthCenter.swift
+++ VCSCommon/VCSNetwork/AuthCenter.swift
@@ -12,17 +12,17 @@ public class AuthCenter {
 }
 
 public extension AuthCenter {
-    func updateLogginSettings(redirectURI: String? = nil, sharedGroup: String? = nil, retryClearDataBlock: @escaping () -> Void, completion: ((Result<VCSLoginSettingsResponse, Error>) -> Void)? = nil) {
+    func updateLogginSettings(redirectURI: String? = nil, sharedGroup: String? = nil, completion: ((Result<VCSLoginSettingsResponse, Error>) -> Void)? = nil) {
         APIClient.loginSettings().execute(onSuccess: { (result: VCSLoginSettingsResponse) in
             AuthCenter.shared.loginSettings = result
-            APIClient.updateOAuthClient(loginSettings: result, retryClearDataBlock: retryClearDataBlock, redirectURI: redirectURI, sharedGroup: sharedGroup)
+            APIClient.updateOAuthClient(loginSettings: result, redirectURI: redirectURI, sharedGroup: sharedGroup)
             completion?(.success(result))
         }, onFailure: { (error: Error) in
             completion?(.failure(error))
         })
     }
     
-    func loadOAuthFromCache(loginSettings: VCSLoginSettingsResponse, retryClearDataBlock: @escaping () -> Void, sharedGroup: String? = nil) {
-        APIClient.updateOAuthClient(loginSettings: loginSettings, retryClearDataBlock: retryClearDataBlock, sharedGroup: sharedGroup)
+    func loadOAuthFromCache(loginSettings: VCSLoginSettingsResponse, sharedGroup: String? = nil) {
+        APIClient.updateOAuthClient(loginSettings: loginSettings, sharedGroup: sharedGroup)
     }
 }
--- VCSCommon/VCSNetwork/OAuthAlamofireHelpers/OAuth2RetryHandler.swift
+++ VCSCommon/VCSNetwork/OAuthAlamofireHelpers/OAuth2RetryHandler.swift
@@ -6,18 +6,12 @@ import CocoaLumberjackSwift
 /// Add authentification headers from OAuthSwift to Alamofire request
 open class OAuth2RetryHandler: RequestInterceptor {
     
-    public internal(set) var userDidCancelSingIn = false
     let loader: OAuth2DataLoader
     fileprivate var requestsToRetry: [(RetryResult) -> Void] = []
-    private var retryCounter = 4
-    private var retryClearDataBlock: () -> Void = {}
     
-    init?(oauth2: OAuth2C?, retryClearDataBlock: @escaping () -> Void) {
+    init?(oauth2: OAuth2C?) {
         guard let loader = oauth2 else { return nil }
         self.loader = OAuth2DataLoader(oauth2: loader)
-        self.retryClearDataBlock = retryClearDataBlock
-        self.resetRetryCounter()
-        
     }
     
     open func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
@@ -37,42 +31,25 @@ open class OAuth2RetryHandler: RequestInterceptor {
         }
     }
     
-    open func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
-        if self.userDidCancelSingIn == false, let response = request.task?.response as? HTTPURLResponse, 401 == response.statusCode || 403 == response.statusCode, let req = request.request {
-            self.retryCounter = self.retryCounter - 1
-            
-            DDLogError("Retrying 1/3 message - \(request.request?.url)")
-            DDLogError("Retrying 2/3 message - \(response.statusCode)")
-            DDLogError("Retrying 3/3 message - \(error)")
-            
-            if self.retryCounter == 0 {
-                self.retryClearDataBlock()
-                self.resetRetryCounter()
-            }
-            
+    public func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+        if let response = request.task?.response as? HTTPURLResponse, 401 == response.statusCode, let req = request.request, request.error?.isRequestRetryError == false {
             var dataRequest = OAuth2DataRequest(request: req, callback: { _ in })
             
             dataRequest.context = completion
             loader.enqueue(request: dataRequest)
             loader.attemptToAuthorize() { authParams, error in
                 self.loader.dequeueAndApply() { req in
-                    if self.retryCounter > 0 {
-                        if let comp = req.context as? (RetryResult) -> Void {
-                            comp(nil != authParams ? .retry : .doNotRetry)
+                    if let comp = req.context as? (RetryResult) -> Void {
+                        if error == .requestCancelled {
+                            comp(.doNotRetryWithError(error!))
                         } else {
-                            completion(.doNotRetryWithError(VCSNetworkError.parsingError("RetryResult - cannot parse req.context")))
+                            comp(nil != authParams ? .retry : .doNotRetry)
                         }
-                    } else {
-                        completion(.doNotRetryWithError(VCSNetworkError.parsingError("Too many retries   ")))
                     }
                 }
             }
         } else {
             completion(.doNotRetry)   // not a 401, not our problem
         }
-    }
-    
-    public func resetRetryCounter() {
-        self.retryCounter = 4
     }
 }
