3842152302a03c8ec9b9deb358273e940a3a4eb9
Ivaylo Iliev <iiliev@nemetschek.bg>
2025-02-06 15:46:29 +0200

    MOBILE-8871

--- VCSCommon/FileChooser/FileChooser.swift
+++ VCSCommon/FileChooser/FileChooser.swift
@@ -9,9 +9,9 @@ public struct FileChooser: View {
     @ObservedObject private var viewsLayoutSetting: ViewsLayoutSetting = ViewsLayoutSetting.listDefault
     
     @ObservedObject private var VCSReachabilityMonitor = VCSReachability.default
-        
+    
     @State var fileTypeFilter: FileTypeFilter
-                        
+    
     private var itemPickedCompletion: (RealmFile) -> Void
     
     @Environment(\.dismiss) private var dismiss
@@ -27,7 +27,7 @@ public struct FileChooser: View {
     private var isGuest: Bool {
         users.first?.entity == nil
     }
-        
+    
     private func onItemPicked(pickedModel: FileChooserModel) {
         guard let filePicked = VCSFileResponse.realmStorage.getModelById(id: pickedModel.resourceId) else {
             fatalError("Picked file not found.")
@@ -38,45 +38,43 @@ public struct FileChooser: View {
     
     public var body: some View {
         GeometryReader { geometry in
-            NavigationStack {
-                FileChooserListView(
-                    fileTypeFilter: fileTypeFilter,
-                    itemPickedCompletion: onItemPicked,
-                    onDismiss: { dismiss() },
-                    isGuest: isGuest,
-                    isOnline: VCSReachabilityMonitor.isConnected
-                )
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarBackButtonHidden(true)
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        ToolbarBackButton(
-                            label: "Back".vcsLocalized,
-                            viewWidth: geometry.size.width * 0.3,
-                            onPress: { dismiss() }
-                        )
-                    }
-                    
-                    ToolbarItem(placement: .principal) {
-                        CurrentFilterView(
-                            onDismiss: { dismiss() },
-                            fileTypeFilter: fileTypeFilter
-                        )
-                    }
-                    
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Picker(selection: $viewsLayoutSetting.layout, label: Text("Style")) {
-                            Label(ListLayoutCriteria.grid.buttonName, image: ListLayoutCriteria.grid.buttonImageName)
-                                .tag(ListLayoutCriteria.grid.rawValue)
-                            Label(ListLayoutCriteria.list.buttonName, image: ListLayoutCriteria.list.buttonImageName)
-                                .tag(ListLayoutCriteria.list.rawValue)
-                        }
-                        .tint(Color.label)
-                    }
+            FileChooserListView(
+                fileTypeFilter: fileTypeFilter,
+                itemPickedCompletion: onItemPicked,
+                onDismiss: { dismiss() },
+                isGuest: isGuest,
+                isOnline: VCSReachabilityMonitor.isConnected
+            )
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    ToolbarBackButton(
+                        label: "Back".vcsLocalized,
+                        viewWidth: geometry.size.width * 0.3,
+                        onPress: { dismiss() }
+                    )
                 }
-                .frame(maxWidth: .infinity)
-                .tint(.VCSTeal)
+                
+                ToolbarItem(placement: .principal) {
+                    CurrentFilterView(
+                        onDismiss: { dismiss() },
+                        fileTypeFilter: fileTypeFilter
+                    )
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Picker(selection: $viewsLayoutSetting.layout, label: Text("Style")) {
+                        Label(ListLayoutCriteria.grid.buttonName, image: ListLayoutCriteria.grid.buttonImageName)
+                            .tag(ListLayoutCriteria.grid.rawValue)
+                        Label(ListLayoutCriteria.list.buttonName, image: ListLayoutCriteria.list.buttonImageName)
+                            .tag(ListLayoutCriteria.list.rawValue)
+                    }
+                    .tint(Color.label)
+                }
             }
+            .frame(maxWidth: .infinity)
+            .tint(.VCSTeal)
         }
     }
 }
